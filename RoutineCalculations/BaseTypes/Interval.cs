0727512804827e847aece53abe4c6b76a865a59e
Alex Fridman <AlexFridman@outlook.com>
2015-04-30 20:45:47 +0300

    Класс Интервал реализован

---
+++ RoutineCalculations/BaseTypes/Interval.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace BaseTypes
+{
+    public class Interval<T> where T : IComparable<T>
+    {
+        public T Left { get; set; }
+        public T Right { get; set; }
+        public bool IsLeftClosed { get; set; }
+        public bool IsRightClosed { get; set; }
+
+        public Interval(T left, T right, bool isLeftClosed = false, bool isRightClosed = false)
+        {
+            if (right.CompareTo(left) < 0)
+            {
+                throw new ArgumentException("Left can not be greather than rigth.");
+            }
+
+            Left = left;
+            Right = right;
+            IsLeftClosed = isLeftClosed;
+            IsRightClosed = isRightClosed;
+        }
+
+        public bool Contains(T value)
+        {
+            var leftBorderComparsion = Left.CompareTo(value);
+            var rightBorderComparsion = Right.CompareTo(value);
+
+            if (IsLeftClosed && IsRightClosed)
+            {
+                return leftBorderComparsion <= 0 && rightBorderComparsion >= 0;
+            }
+            if (!IsLeftClosed && IsRightClosed)
+            {
+                return leftBorderComparsion < 0 && rightBorderComparsion >= 0;
+            }
+            if (IsLeftClosed && !IsRightClosed)
+            {
+                return leftBorderComparsion <= 0 && rightBorderComparsion > 0;
+            }
+            return leftBorderComparsion < 0 && rightBorderComparsion > 0;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}{1},{2}{3}",
+                IsLeftClosed ? "[" : "(",
+                Left,
+                Right,
+                IsRightClosed ? "]" : ")");
+        }
+    }
+}
---
+++ RoutineCalculations/Tests/BaseTypes.Tests/IntervalTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using BaseTypes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.BaseTypes.Tests
+{
+    [TestClass]
+    public class IntervalTests
+    {
+        [TestMethod]
+        public void OpenIntervalContainsTest()
+        {
+            // Arrange
+            var interval = new Interval<int>(0, 4, false, false);            
+
+            // Assert
+            Assert.AreEqual(interval.Contains(3), true);
+            Assert.AreEqual(interval.Contains(5), false);
+        }
+
+        [TestMethod]
+        public void LeftClosedIntervalContainsTest()
+        {
+            // Arrange
+            var interval = new Interval<int>(0, 4, true, false);
+
+            // Assert
+            Assert.AreEqual(interval.Contains(0), true);
+            Assert.AreEqual(interval.Contains(2), true);
+            Assert.AreEqual(interval.Contains(-1), false);
+        }
+
+        [TestMethod]
+        public void RigthClosedIntervalContainsTest()
+        {
+            // Arrange
+            var interval = new Interval<int>(0, 4, false, true);
+
+            // Assert
+            Assert.AreEqual(interval.Contains(4), true);
+            Assert.AreEqual(interval.Contains(2), true);
+            Assert.AreEqual(interval.Contains(5), false);
+        }
+
+
+        [TestMethod]
+        public void ClosedIntervalContainsTest()
+        {
+            // Arrange
+            var interval = new Interval<int>(0, 4, true, true);
+
+            // Assert
+            Assert.AreEqual(interval.Contains(2), true);
+            Assert.AreEqual(interval.Contains(0), true);
+            Assert.AreEqual(interval.Contains(4), true);
+            Assert.AreEqual(interval.Contains(-1), false);
+            Assert.AreEqual(interval.Contains(5), false);
+        }
+
+    }
+}
