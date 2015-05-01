e79d72095cdc55e40b73b6affd7882ced68128ee
Alex Fridman <AlexFridman@outlook.com>
2015-05-01 14:32:18 +0300

    Класс DecimalFrequencyFunction реализован

---
+++ RoutineCalculations/BaseTypes/DecimalFrequencyFunction.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace BaseTypes
+{
+    public class DecimalFrequencyFunction
+    {
+        private readonly IEnumerable<decimal> _statisticalSeries;
+        private readonly int _selectionLength;
+        public IReadOnlyCollection<Item> Frequencies { get; private set; }
+        public IReadOnlyCollection<Point> Function { get; private set; }
+
+        public DecimalFrequencyFunction(IEnumerable<decimal> statisticalSeries)
+        {
+            if (statisticalSeries == null)
+            {
+                throw new ArgumentNullException("statisticalSeries");
+            }
+
+            _statisticalSeries = statisticalSeries.OrderBy(i => i);
+            _selectionLength = _statisticalSeries.Count();
+
+            CalculateFrequencies();
+            BuildFunction();
+        }
+
+        public struct Item
+        {
+            public override string ToString()
+            {
+                return string.Format("Value: {0}, Frequency: {1}", Value, Frequency);
+            }
+
+            public decimal Value { get; set; }
+            public decimal Frequency { get; set; }
+        }
+
+        public struct Point
+        {
+            public decimal X { get; set; }
+            public decimal Y { get; set; }
+
+            public override string ToString()
+            {
+                return string.Format("X: {0}, Y: {1}", X, Y);
+            }
+        }
+
+        private void CalculateFrequencies()
+        {
+            var steps =
+                _statisticalSeries.GroupBy(i => i)
+                    .Select(
+                        group => new Item {Value = @group.Key, Frequency = @group.Count()/(decimal) _selectionLength})
+                    .OrderBy(s => s.Value).ToList();
+
+            Frequencies = new ReadOnlyCollection<Item>(steps);
+        }
+
+        private void BuildFunction()
+        {
+            var points = new List<Point>(Frequencies.Count);
+
+            decimal currentValue = 0;
+
+            foreach (var item in Frequencies)
+            {
+                currentValue += item.Frequency;
+                var point = new Point {X = item.Value, Y = currentValue};
+
+                points.Add(point);
+            }
+
+            Function = new ReadOnlyCollection<Point>(points);
+        }
+    }
+}
---
+++ RoutineCalculations/Tests/BaseTypes.Tests/DecimalFrequencyFunctionTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using BaseTypes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.BaseTypes.Tests
+{
+    [TestClass]
+    public class DecimalFrequencyFunctionTests
+    {
+        [TestMethod]
+        public void ReturnsCorrectFunction()
+        {
+            // Arrange
+            var data = new decimal[]
+            {-20, -60, -10, 30, 60, 70, -10, -30, 120, -100, -80, 20, 40, -60, -10, 20, 30, -80, 60, 70};
+            var freqFunc = new DecimalFrequencyFunction(data);
+
+            // Assert
+            Assert.AreEqual(freqFunc.Function.First().Y, 0.05m);
+            Assert.AreEqual(freqFunc.Function.Skip(1).First().Y, 0.15m);
+            Assert.AreEqual(freqFunc.Function.Last().Y, 1m);         
+        }
+    }
+}
