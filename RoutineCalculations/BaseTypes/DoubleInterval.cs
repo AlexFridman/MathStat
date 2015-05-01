134978d744a4efaf143b816c186ee60cb3e07068
Alex Fridman <AlexFridman@outlook.com>
2015-05-01 18:26:17 +0300

    Класс DOubleInterval реализован

---
+++ RoutineCalculations/BaseTypes/DoubleInterval.cs
@@ -0,0 +1,20 @@
+﻿namespace BaseTypes
+{
+    public class DoubleInterval : Interval<double>
+    {
+        public double Middle
+        {
+            get { return Left + (Right - Left)/2; }
+        }
+
+        public double Length
+        {
+            get { return (Right - Left); }
+        }
+
+        public DoubleInterval(double left, double right, bool isLeftClosed = false, bool isRightClosed = false)
+            : base(left, right, isLeftClosed, isRightClosed)
+        {
+        }
+    }
+}
